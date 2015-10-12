62704f19f856e3a42d10f455cdd6358483156ca0
Thiago Augustus <thiagoaugustusdeoliveira@gmail.com>
2015-10-12 17:17:57 -0300

    Update main

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "github.com/thiagoaugustus/GoLang-Goroutines/functions"
 
 func main() {
-	functions.printme(2)
+	// Print in sequence
+	functions.PrintMe(1)
+	functions.PrintMe(2)
+	// Treats as threads
+	go functions.PrintMe(3)
+	functions.PrintMe(4)
+	// The main function will end earlier than goroutines
+	go functions.PrintMe(5)
+	go functions.PrintMe(6)
 }
